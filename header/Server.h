acf20e6d2b45d848dc313c8d8c2c161a02210e8d
Christian Paling <christian.paling@googlemail.com>
2017-04-22 17:26:36 +0200

    Add basic tcp server structure

--- header/Server.h
+++ header/Server.h
@@ -1,8 +1,15 @@
 #ifndef C_SERVER_H
 #define C_SERVER_H
 
+#include <boost/asio/io_service.hpp>
+#include <iostream>
+
 class Server {
 public:
     Server();       //constructor
+    void run();
+
+private:
+    boost::asio::io_service io_service;
 };
 #endif //C_SERVER_H
