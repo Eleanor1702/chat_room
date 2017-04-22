51ebfa1cc5bb4c65f0beda5fbf7f20c8c866eb5a
Christian Paling <christian.paling@googlemail.com>
2017-04-22 18:10:24 +0200

    Implement basic server connection accept

--- header/Server.h
+++ header/Server.h
@@ -1,8 +1,11 @@
 #ifndef C_SERVER_H
 #define C_SERVER_H
 
-#include <boost/asio/io_service.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <iostream>
+
+#define PORT_NUMBER 8000
 
 class Server {
 public:
@@ -10,6 +13,10 @@ public:
     void run();
 
 private:
+    void new_message();
+
     boost::asio::io_service io_service;
+    boost::asio::ip::tcp::acceptor acceptor;
+    boost::asio::ip::tcp::socket socket;
 };
 #endif //C_SERVER_H
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,7 +1,7 @@
 #include "../header/Server.h"
 
-Server::Server() {      //constructor
-
+Server::Server() : acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PORT_NUMBER)), socket(io_service) {      //constructor
+    acceptor.async_accept(socket, boost::bind(&Server::new_message, this));
 }
 
 void Server::run() {
@@ -11,4 +11,8 @@ void Server::run() {
     catch(std::exception& e) {
         std::cerr << e.what() << std::endl; 
     }
+}
+
+void Server::new_message() {
+    std::cout << "Received!!" << std::endl;
 }
