1be03a59d1d878bcf56aee4c9d2dc2965a9230d6
Noor Alrabea <eleanor.alrabea@gmail.com>
2017-04-22 17:39:28 +0200

    requesting Server Credentials

--- header/User.h
+++ header/User.h
@@ -6,8 +6,11 @@ class User {
 public:
     User();     //constructor
     void req_name();
+    void req_server_credentials();
 private:
     std::string username;
+    std::string server_ip;
+    std::string server_port;
 };
 
 #endif //C_USER_H
